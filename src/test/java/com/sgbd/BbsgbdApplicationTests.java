e6c8adccbe981f234bb41a819542be5767389dcd
Lucas Lopes <lucasslopes2004@gmail.com>
2024-05-23 09:01:41 -0300

    primeiro commit

---
+++ src/test/java/com/sgbd/BbsgbdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgbd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbsgbdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
