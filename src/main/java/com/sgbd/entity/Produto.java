b1049c48d461cc221d15794850eb8be3ad660304
lucas lopes <lucasslopes2004@gmail.com>
2024-05-31 23:38:29 -0300

    Adicionando entidades

--- src/main/java/com/sgbd/entity/Produto.java
+++ src/main/java/com/sgbd/entity/Produto.java
@@ -1,4 +1,35 @@
 package com.sgbd.entity;
 
-public class Produto {
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "Produtos")
+@Data
+@Getter
+@Setter
+public class Produto implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+    
+    @Column(nullable = false)
+    private String marca;
+
+    @Column(nullable = false)
+    private Integer quantidade;
+
 }
